e141254e69e8ec8f0ec0ed565ffaeb383be8174f
Arjun <arjunk@thoughtworks.com>
2017-08-25 08:24:39 +0530

    search for draft purchase orders

--- src/main/java/org/bahmni/erp/reader/Application.java
+++ src/main/java/org/bahmni/erp/reader/Application.java
@@ -3,7 +3,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Vector;
-import java.util.logging.Logger;
 
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
@@ -20,6 +19,7 @@ public class Application {
         System.out.println(databaseList);
         int connect = app.connect("local", 8069, "openerp", "admin", "password");
         System.out.println(connect);
+        app.search("local",8069, connect);
 
     }
 
@@ -85,22 +85,56 @@ public class Application {
         }
     }
 
-    public void search(String host, int port) throws MalformedURLException {
-        XmlRpcClient xmlrpcLogin = new XmlRpcClient();
+    public void search(String host, int port, Integer uid) throws MalformedURLException {
+        XmlRpcClient xmlrpcClient = getXmlRpcClient(host, port);
 
-        XmlRpcClientConfigImpl xmlrpcConfigLogin = new XmlRpcClientConfigImpl();
-        xmlrpcConfigLogin.setEnabledForExtensions(true);
-        xmlrpcConfigLogin.setServerURL(new URL("http",host,port,"/xmlrpc/object"));
+//        Object[] names = new Object[] {"state", "=", "draft"};
+        Object[] params = new Object[] {"openerp",uid,"password", "purchase.order", "search"};
 
-        xmlrpcLogin.setConfig(xmlrpcConfigLogin);
+        Object r[]=new Object[6];
+        r[0]="openerp";
+        r[1]=(Integer)uid;
+        r[2]="password";
+        r[3]="purchase.order";
+        r[4]="search";
 
-        Object[] params = new Object[] {"openerp","admin","password"};
+        Object names[]=new Object[3];
+
+
+
+        names[0]="state";
+        names[1]="=";
+        names[2] = "draft";
+
+        Vector param2 = new Vector();
+        param2.addElement(names);
+        r[5]=param2;
+        Object po_ids;
         try {
-            Object id = xmlrpcLogin.execute("search", params);
+            po_ids=xmlrpcClient.execute("execute", r);
+            Object[] d = (Object[]) po_ids;
+
+            Integer i=0;
+
+            for(i=0; i<d.length; i++){
+                System.out.println((Integer)d[i]);
+            }
+
 
         } catch (XmlRpcException e) {
             e.printStackTrace();
         }
+    }
+
+    private XmlRpcClient getXmlRpcClient(String host, int port) throws MalformedURLException {
+        XmlRpcClient xmlrpcClient = new XmlRpcClient();
+
+        XmlRpcClientConfigImpl xmlrpcConfig = new XmlRpcClientConfigImpl();
+        xmlrpcConfig.setEnabledForExtensions(true);
+        xmlrpcConfig.setServerURL(new URL("http",host,port,"/xmlrpc/object"));
+
+        xmlrpcClient.setConfig(xmlrpcConfig);
+        return xmlrpcClient;
     }
 
     private Object [] read(XmlRpcClient xmlRpcLogin, String dbName, int uid, String password, String table, ArrayList<Object> ids, ArrayList<Object>fields) throws XmlRpcException{
